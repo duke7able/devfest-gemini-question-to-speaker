1de3dbad24123f17e843d906dd3cc9ef57b7975e
Shreyan Mehta <shreyan@Shreyans-MacBook-Air.local>
2024-12-25 16:13:58 +0530

    feat : added appscript repo link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,11 +19,18 @@ export default function Footer() {
           </Link>
         </p>
         <Link
+          href="https://github.com/duke7able/gemini-appscript"
+          className="flex items-center text-muted-foreground hover:text-primary"
+        >
+          <Github className="w-5 h-5 mr-2" />
+          <span>Appscript repo Github</span>
+        </Link>
+        <Link
           href="https://github.com/duke7able/devfest-gemini-question-to-speaker"
           className="flex items-center text-muted-foreground hover:text-primary"
         >
           <Github className="w-5 h-5 mr-2" />
-          <span>GitHub</span>
+          <span>This repo Github</span>
         </Link>
       </div>
     </footer>
